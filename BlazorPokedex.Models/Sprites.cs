80f37d815649a9914ceba81c84b82849de38e524
BA\lamerio <lamerio@tinsa.ar>
2023-08-22 09:40:38 -0300

    push full app

---
+++ BlazorPokedex.Models/Sprites.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace BlazorPokedex.Models
+{
+    public class Sprites
+    {
+        [JsonProperty("front_default")]
+        public string FronDefault { get; set; }
+
+        [JsonProperty("back_default")]
+        public string Back { get; set; }
+    }
+}
